44941f73aa14e7d3eecf1485e90c3a04fda7c67a
Cameron Taylor <TheJimmyBlaze@gmail.com>
2019-04-19 18:35:20 +1200

    Updated Message Page
    Added some more controls to message page, including a text box for entering messages, and itemcontrol data templates for connected users.

---
+++ ExampleClient/Domain/DisplayableAccount.cs
@@ -0,0 +1,40 @@
+﻿using JamLib.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ExampleClient.Domain
+{
+    public struct DisplayableAccount
+    {
+        public Account Account { get; set; }
+        public DisplayableAccount(Account account)
+        {
+            Account = account;
+        }
+
+        public string Initial
+        {
+            get { return Account.Username.Substring(0, 1); }
+            set { }
+        }
+
+        public Brush Colour
+        {
+            get
+            {
+                Random random = new Random(Account.AccountID.GetHashCode());
+                int r = (int)(random.NextDouble() * 100) + 100;
+                int g = (int)(random.NextDouble() * 100) + 100;
+                int b = (int)(random.NextDouble() * 100) + 100;
+
+                return new SolidColorBrush(Color.FromArgb(255, (byte)r, (byte)g, (byte)b));
+            }
+            set { }
+        }
+    }
+}
--- ExampleClient/View/MessagePage.xaml.cs
+++ ExampleClient/View/MessagePage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ExampleClient.Domain;
+using JamLib.Database;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +22,58 @@ namespace ExampleClient.View
     /// <summary>
     /// Interaction logic for MessagePage.xaml
     /// </summary>
-    public partial class MessagePage : Page
+    public partial class MessagePage : Page, INotifyPropertyChanged
     {
+        private ObservableCollection<DisplayableAccount> accounts;
+        public ObservableCollection<DisplayableAccount> Accounts
+        {
+            get
+            {
+                if (accounts == null)
+                    accounts = new ObservableCollection<DisplayableAccount>();
+                return accounts;
+            }
+            set { }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public MessagePage()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            Accounts.Add(new DisplayableAccount(new Account()
+            {
+                AccountID = Guid.Parse("f29af9c5-c98f-4067-a3ea-f9baab8853f9"),
+                LastUpdateUTC = DateTime.UtcNow,
+                Username = "Baiias",
+                Approved = true
+            }));
+
+            Accounts.Add(new DisplayableAccount(new Account()
+            {
+                AccountID = Guid.Parse("67a7f709-7f6d-45da-bde6-2284fdfd6750"),
+                LastUpdateUTC = DateTime.UtcNow,
+                Username = "Deadponys",
+                Approved = false
+            }));
+
+            Accounts.Add(new DisplayableAccount(new Account()
+            {
+                AccountID = Guid.Parse("ad509e39-afa9-4b56-bfe6-6c9d70a50954"),
+                LastUpdateUTC = DateTime.UtcNow,
+                Username = "TheJimmyBlaze",
+                Approved = true
+            }));
+        }
+
+        public void NotifyPropertyChanged(string name = "")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
